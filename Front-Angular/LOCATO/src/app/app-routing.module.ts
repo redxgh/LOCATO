1daf8450410468059d9e0a9c2adc23d0040b7c03
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2023-10-27 17:56:23 +0100

    module post ad with starting post ad

--- Front-Angular/LOCATO/src/app/app-routing.module.ts
+++ Front-Angular/LOCATO/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostAdComponent } from './post-ad/post-ad/post-ad.component';
+import { ListAdsComponent } from './components/list-ads/list-ads.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component:ListAdsComponent},
+  { path: 'post-ad', component: PostAdComponent, children:[
+    // { path: '', redirectTo: 'profile', pathMatch: 'full' }, // Default route
+    {path:'',component:ListAdsComponent},
+
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
