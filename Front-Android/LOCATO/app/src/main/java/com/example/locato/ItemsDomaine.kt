d80c398f7c8516d04c020ed1b6cc5e4dd5339914
sarahhamza <sarahhm31@gmail.com>
2023-11-14 23:25:00 +0100

    detailsActivity

--- Front-Android/LOCATO/app/src/main/java/com/example/locato/ItemsDomaine.kt
+++ Front-Android/LOCATO/app/src/main/java/com/example/locato/ItemsDomaine.kt
@@ -5,7 +5,14 @@ import java.io.Serializable
 class ItemsDomaine(
     val titleTxt: String,
     val address: String,
+    val description: String,
+    val bed: Int,
+    val bath: Int,
     val price: Int,
-    val pic: String
+    val pic: String,
+    val wifi: Boolean
+
+
+
 ) : Serializable {
 }
