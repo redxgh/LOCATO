0592e1caf8c39f544bd8c6f58c7df190388b7131
sarahhamza <sarahhm31@gmail.com>
2023-11-14 20:09:06 +0100

    bottomBar

--- Front-Android/LOCATO/app/src/main/java/com/example/locato/HomeActivity.kt
+++ Front-Android/LOCATO/app/src/main/java/com/example/locato/HomeActivity.kt
@@ -1,8 +1,8 @@
 package com.example.locato
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,27 +11,46 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var recyclerViewPopular: RecyclerView
+    private lateinit var recyclerViewNew: RecyclerView
+
     private lateinit var itemsAdapterPopular: ItemsAdapter
+    private lateinit var itemsAdapterNew: ItemsAdapter
+
     private val itemsListPopular: ArrayList<ItemsDomaine> = ArrayList()
+    private val itemsListNew: ArrayList<ItemsDomaine> = ArrayList()
+
     private lateinit var postAdBtn : FloatingActionButton
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.scroll_main)
         // Initialisation du RecyclerView et ajout des éléments à la liste itemsListPopular
         recyclerViewPopular = findViewById(R.id.viewPopular)
+        recyclerViewNew = findViewById(R.id.viewNew)
+
 
         // Ajout d'exemples d'éléments à la liste itemsListPopular
         val item1 = ItemsDomaine("Dar Calibori", "Sousse", 100, "pic1")
         val item2 = ItemsDomaine("Plermio", "Sud", 200, "pic2")
+        val item3 = ItemsDomaine("Dar Maria", "Cap zebib", 150, "pic3")
+
         itemsListPopular.add(item1)
         itemsListPopular.add(item2)
+        itemsListNew.add(item3)
+
 
         // Configuration du RecyclerView avec un LayoutManager et l'adaptateur
         itemsAdapterPopular = ItemsAdapter(itemsListPopular)
         recyclerViewPopular.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerViewPopular.adapter = itemsAdapterPopular
+
+        itemsAdapterNew = ItemsAdapter(itemsListNew)
+        recyclerViewNew.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewNew.adapter = itemsAdapterNew
+
+
 
         postAdBtn = findViewById(R.id.postAdBtn)
         postAdBtn.setOnClickListener(){
