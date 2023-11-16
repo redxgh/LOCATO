dfd80606486d077bc5aaf1b330789f47ae7923c3
habib <dhaouadimedhbib@gmail.com>
2023-11-16 23:50:35 +0100

    adding ad-service

--- Front-Angular/LOCATO/src/app/components/list-ads/list-ads.component.ts
+++ Front-Angular/LOCATO/src/app/components/list-ads/list-ads.component.ts
@@ -2,6 +2,7 @@ import { Component, AfterViewInit } from '@angular/core';
 import { Ad } from 'src/app/model/Ad';
 import * as $ from 'jquery';
 import * as Aos from 'aos';
+import { AdService } from 'src/app/ad.service';
 
 @Component({
   selector: 'app-list-ads',
@@ -9,108 +10,12 @@ import * as Aos from 'aos';
   styleUrls: ['./list-ads.component.scss'],
 })
 export class ListAdsComponent implements AfterViewInit {
-  ads: Ad[] = [
-    {
-      id: 1,
-      title: 'Luxury Apartment in Downtown',
-      description: 'Spacious 2-bedroom apartment with a great view.',
-      price: 1500,
-      timestamp: 'month',
-      accommodationId: 101,
-      userId: 201,
-      images: ['house3.jpg', 'house2.jpg', 'house4.jpg'],
-    },
-    {
-      id: 2,
-      title: 'Cozy Room in Shared House',
-      description: 'A comfortable room in a friendly shared house.',
-      price: 50,
-      timestamp: 'night',
-      accommodationId: 102,
-      userId: 202,
-      images: ['house4.jpg', 'house3.jpg', 'house4.jpg'],
-    },
-    {
-      id: 3,
-      title: 'Roommate Wanted for Spacious Condo',
-      description: 'Looking for a roommate to share a beautiful condo.',
-      price: 800,
-      timestamp: 'month',
-      accommodationId: 103,
-      userId: 203,
-      images: ['house3.jpg', 'house2.jpg', 'house4.jpg'],
-    },
-    {
-      id: 4,
-      title: 'Charming Single Room for Rent',
-      description: 'Single room available in a lovely house.',
-      price: 60,
-      timestamp: 'night',
-      accommodationId: 104,
-      userId: 204,
-      images: ['house1.jpg', 'house3.jpg', 'house4.jpg'],
-    },
-    {
-      id: 5,
-      title: 'Spacious House with Garden',
-      description: '3-bedroom house with a beautiful garden and pool.',
-      price: 2000,
-      timestamp: 'month',
-      accommodationId: 105,
-      userId: 205,
-      images: ['house2.jpg', 'house4.jpg'],
-    },
-    {
-      id: 1,
-      title: 'Luxury Apartment in Downtown',
-      description: 'Spacious 2-bedroom apartment with a great view.',
-      price: 1500,
-      timestamp: 'month',
-      accommodationId: 101,
-      userId: 201,
-      images: ['house4.jpg', 'house4.jpg'],
-    },
-    {
-      id: 2,
-      title: 'Cozy Room in Shared House',
-      description: 'A comfortable room in a friendly shared house.',
-      price: 50,
-      timestamp: 'night',
-      accommodationId: 102,
-      userId: 202,
-      images: ['house1.jpg', 'house2.jpg'],
-    },
-    {
-      id: 3,
-      title: 'Roommate Wanted for Spacious Condo',
-      description: 'Looking for a roommate to share a beautiful condo.',
-      price: 800,
-      timestamp: 'month',
-      accommodationId: 103,
-      userId: 203,
-      images: ['house1.jpg', 'house2.jpg', 'house3.jpg', 'house4.jpg'],
-    },
-    {
-      id: 4,
-      title: 'Charming Single Room for Rent',
-      description: 'Single room available in a lovely house.',
-      price: 60,
-      timestamp: 'night',
-      accommodationId: 104,
-      userId: 204,
-      images: ['house1.jpg', 'house2.jpg'],
-    },
-    {
-      id: 5,
-      title: 'Spacious House with Garden',
-      description: '3-bedroom house with a beautiful garden and pool.',
-      price: 2000,
-      timestamp: 'month',
-      accommodationId: 105,
-      userId: 205,
-      images: ['house2.jpg'],
-    },
-  ];
+
+  ads:Ad[];
+  constructor(private adService : AdService){
+    this.ads=adService.listAds();
+  }
+  
   ngAfterViewInit() {
     //sliders
     $('.slider').each(function () {
@@ -119,18 +24,16 @@ export class ListAdsComponent implements AfterViewInit {
       var slideHeight = $(this).find('ul li').height();
       var sliderUlWidth = slideCount * slideWidth;
 
-      // If there's only one image, don't set up the slider functionality
+      // idha ma3ana hata image bch y'affichi just el tawira loula
       if (slideCount === 1) {
         $(this).find('ul li').show(); // show the single image
         $(this).find('.control_next').hide();
         $(this).find('.control_prev').hide();
-
         return;
       }
       $(this).find('ul').css({ width: sliderUlWidth, marginLeft: -slideWidth });
 
       $(this).find('ul li:last-child').prependTo($(this).find('ul'));
-
       function moveLeft(slider: JQuery<any>) {
         var sliderUl = $(slider).find('ul');
         sliderUl.animate(
